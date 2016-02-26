6f1c7204f6b9f7e1bd3871254166a6285bf8a3ed
scoophamp <hampus_cousin@hotmail.com>
2016-02-26 15:11:47 +0100

    Added files via upload

---
+++ Test/Funktion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Funktion
+    {
+        private string löss = string.Empty;
+        private string möss = string.Empty;
+
+        public Funktion(string Löss, string Möss)
+        {
+        this.löss = Löss;
+            this.möss = Möss;
+
+        }
+        public Funktion(string kiss, string bajs, string hej)
+
+
+        {
+            List<string> Djur = new List<string>();
+           
+
+            Djur.Add("Bamsebjörn");
+            Djur.Add("Kaninskit");
+            Djur.Add("Fjäderfä");
+            Djur.Add("Kanonkamrat");
+
+          
+        }
+       
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        public static string Löss;
+        public static string Möss;
+
+        static void Main(string[] args)
+        {
+
+            string namn = string.Empty;
+
+            Console.WriteLine("Skriv ett namn");
+            namn = Console.ReadLine();
+
+            
+            {
+
+            }
+          switch (namn)
+            {
+                case "Sten":
+                    Console.WriteLine("Hej sten, du är ful");
+                    Console.WriteLine(DateTime.Now);
+                    Funktion Djur = new Funktion(Löss, Möss);
+                    
+                    break;
+
+                default:
+                    Console.WriteLine("Du skrev inte Sten");
+                    break;
+
+                   
+            }
+            Console.ReadKey();
+        }
+    }
+}
